36362170cc4251a53ec0543d1654d605a0cca6dc
aneesnv <aneesnv@gmail.com>
2018-12-21 23:41:02 +0300

    Added country with name as per #2

--- Test.py
+++ Test.py
@@ -1,7 +1,6 @@
 name = "Aneesh"
+country ="India"
 i = 0
-while i <5: #Loop
-  print(name)
+while i <5: #Loop 5 times
+  print('Your Name is {}'.format(name),'Your Country is {}'.format(country,))
   i+= 1
-  
-
