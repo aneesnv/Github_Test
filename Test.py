4da16048619a8acbcb0308ad6037c49a5bac4aac
aneesnv <aneesnv@gmail.com>
2018-12-22 07:44:35 +0300

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,6 +1,6 @@
 name = "Aneesh"
 country ="India"
 i = 0
-while i <5: #Loop 5 times
+while i <10: #Loop 5 times
   print('Your Name is {}'.format(name),'Your Country is {}'.format(country))
   i+= 1
