86312c1e7320e6768f10b679f2cab6161e6feebe
aneesnv <aneesnv@gmail.com>
2018-12-22 09:43:37 +0300

    with window

--- Test.py
+++ Test.py
@@ -1,6 +1,4 @@
-name = "Aneesh"
-country ="India"
-i = 0
-while i <10: #Loop 5 times
-  print('Your Name is {}'.format(name),'Your Country is {}'.format(country))
-  i+= 1
+from tkinter import *
+window =Tk()
+window.title("Aneesh Window")
+window.mainloop()
